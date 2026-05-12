47cfe47728a80f7b4f24a167ac019b508bf8405a
PavloMotliuk <motlukp@gmail.com>
2026-05-12 15:51:29 +0300

    feat: створення структури директорій (components, pages, data) та додавання мокових даних рейсів

---
+++ src/data/trains.js
@@ -0,0 +1,50 @@
+export const trainsData = [
+  {
+    id: 1,
+    trainNumber: "091К",
+    departureCity: "Львів",
+    arrivalCity: "Київ",
+    departureDateTime: "2026-05-12 22:15",
+    duration: "7 год 20 хв"
+  },
+  {
+    id: 2,
+    trainNumber: "715К (Інтерсіті+)",
+    departureCity: "Київ",
+    arrivalCity: "Львів",
+    departureDateTime: "2026-05-13 06:46",
+    duration: "6 год 10 хв"
+  },
+  {
+    id: 3,
+    trainNumber: "105Ш",
+    departureCity: "Одеса",
+    arrivalCity: "Київ",
+    departureDateTime: "2026-05-13 21:30",
+    duration: "9 год 40 хв"
+  },
+  {
+    id: 4,
+    trainNumber: "017О",
+    departureCity: "Харків",
+    arrivalCity: "Львів",
+    departureDateTime: "2026-05-14 14:00",
+    duration: "13 год 15 хв"
+  },
+  {
+    id: 5,
+    trainNumber: "043Л",
+    departureCity: "Івано-Франківськ",
+    arrivalCity: "Київ",
+    departureDateTime: "2026-05-14 20:00",
+    duration: "10 год 30 хв"
+  },
+  {
+    id: 6,
+    trainNumber: "732Ш (Інтерсіті+)",
+    departureCity: "Київ",
+    arrivalCity: "Дніпро",
+    departureDateTime: "2026-05-15 16:10",
+    duration: "6 год 05 хв"
+  }
+];
